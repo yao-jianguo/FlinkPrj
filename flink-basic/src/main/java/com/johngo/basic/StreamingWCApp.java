80738dcbab9638817b252bd2c9721886c302edb2
Johngo <johngo689@163.com>
2022-03-13 23:04:04 +0800

    Initial commit

---
+++ flink-basic/src/main/java/com/johngo/basic/StreamingWCApp.java
@@ -0,0 +1,10 @@
+package com.johngo.basic;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamingWCApp {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.addSource()
+    }
+}
---
+++ flink-datastream/src/main/java/com/johngo/source/StreamAPP.java
@@ -0,0 +1,9 @@
+package com.johngo.source;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamAPP {
+    public static void main(String[] args) {
+        StreamExecution.
+    }
+}
